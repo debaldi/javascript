4e99f1039e17bbc4599c5c0a50b4900273da1a59
Emerson de Baldi <edbemerson@gmail.com>
2022-02-21 17:48:14 -0400

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+
+/* CONDIÇÃO SIMPLES */
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre com cindo de segurança!`)
