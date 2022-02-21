379007644e9539ddf99658bd8aae5b4bba0d3869
Emerson de Baldi <edbemerson@gmail.com>
2022-02-21 19:28:03 -0400

    update

--- aula11/js010.js
+++ aula11/js010.js
@@ -1,18 +1,27 @@
 function calcular(){
     var txtv = window.document.getElementById('txtvel')
-    var res = document.querySelector('div#res')
+    var resVel = document.querySelector('div#resVel')
     
     // Faz o tratamento da variavel txtv para converter de String para Números
     var velocidade = Number(txtv.value)
 
 
-    res.innerHTML = `<p>A Velocidade do seu carro é <strong> ${velocidade}km/h </strong></p>`
+    resVel.innerHTML = `<p>A Velocidade do seu carro é <strong> ${velocidade}km/h </strong></p>`
 
     if (velocidade > 80 ){
-        res.innerHTML += '<p>Você ultrapassou a velocidade permitida de 80km/h. <strong> MULTADO</strong> </p>'
+        resVel.innerHTML += '<p>Você ultrapassou a velocidade permitida de 80km/h. <strong> MULTADO</strong> </p>'
     }
 
-    res.innerHTML += '<p>Dirija sempre com cinto de segurança!</p>'
+    resVel.innerHTML += '<p>Dirija sempre com cinto de segurança!</p>'
+}
 
-    
+function nacionalidade(){
+    var pais = window.document.getElementById('txtNacio')
+    var resNac = document.getElementById('resNac')
+
+    if(pais.value.toUpperCase() == 'BRASIL'){
+        resNac.innerHTML = `<p>Você é Brasileiro</p>`
+    }else{
+        resNac.innerHTML = `<p>Você é Estrangeito</p>`
+    }
 }
