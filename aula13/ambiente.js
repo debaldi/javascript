e56a19dc6595f7355ffb4b1239a4cb2e89ac5aa6
Emerson de Baldi <edbemerson@gmail.com>
2022-02-22 20:32:33 -0400

    update

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+//Estrutura de Repetição
+
+// while
+var c = 1
+while( c <= 6 ){
+     
+    console.log(`while ${c}`)
+    c++
+
+}
+
+// do While
+var cont = 1
+do{
+    console.log(`do While ${cont}`)
+    cont ++
+} while (cont <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,8 @@
+// Estrutura de Repetição com variavel de controle
+// For
+
+console.log('Começo')
+for(var c = 1; c <= 5; c++ ){
+    console.log(c)
+}
+console.log('FIM')
