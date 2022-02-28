eb26ff7ff8b2bb04aa33906651d5c9b3f7fd35da
Emerson de Baldi <edbemerson@gmail.com>
2022-02-27 21:32:56 -0400

    update

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,17 +1,29 @@
+// Cia um vetor vazio
+let vetorNum = []
+
+
 function adicionar(){
     let num = document.getElementById('txtnum')
     let res = document.getElementById('res')
     let selnum = document.getElementById('selnum')
 
-    let vetorNum = []
+    //Criando um elemento <option> dentro do select
+    let item = document.createElement('option')        
 
-    vetorNum.push(num.value) 
-
-    let item = document.createElement('option')    
-    
-    item.text = vetorNum
-
+    //Criando o elemento item dentro do <option> no select
     selnum.appendChild(item)
+ 
+    //Adicionando o numero digitado dentro do elemento item no select
+    item.text += num.value
 
+    //Apaga o que esta no input num
     num.value = ''
+
+    //Números adicionados no ultimo elemento do vetor
+    vetorNum.push(num.value)
+
+    //Contando quantos elementos tem dentro do vetor
+    let tamanho = vetorNum.length
+    res.innerText = tamanho
+
 }
---
+++ aula16ex/ex018/teste.js
@@ -0,0 +1,16 @@
+
+// Cia um vetor vazio
+let vetorNum = []
+let num = 12
+
+//Números adicionados no ultimo elemento do vetor
+vetorNum.push(12)
+
+
+console.log(vetorNum)
+
+//Contando quantos elementos tem dentro do vetor
+let tamanho = vetorNum.length
+console.log(tamanho)
+
+
