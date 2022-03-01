f8850de6657e6fde3e4dbd43cb1498a34ad53243
Emerson de Baldi <edbemerson@gmail.com>
2022-03-01 00:28:29 -0400

    Analisador de Números SCRIPT.JS

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -5,11 +5,28 @@ let num = document.getElementById('txtnum')
 let res = document.getElementById('res')
 let selnum = document.getElementById('selnum')
 
-
 function adicionar(){
-    if(Number(num.value) < 1 || Number(num.value) > 100 ){
+    /*
+    Verificando se já existe o número digitado dentro do vetor
+    se já contém o número digitado dentro do vetor, a variavel numDiferente recebe false
+    se o número digitado não tiver dentro do vetor, a variavel numDiferente recebe true
+    */
+    let numDiferente = true
+
+    for(let pos in vetorNum){
+        if(vetorNum[pos] == num.value){
+            numDiferente = false
+        window.alert(`Já Existe o número ${vetorNum[pos]} cadastrado!`)
+        } 
+    }
+
+    //Verificando se o número digitado esta entre 1 e 100
+    if( Number(num.value) < 1 || Number(num.value) > 100 ){
         window.alert('Somente Números entre 1 e 100!')
-    } else {
+    
+    //Se acaso o número digitado não existir dentro do vetor e estiver entre 1 e 100
+    } else if (numDiferente == true) {
+        
         //Criando um elemento <option> dentro do select
         let item = document.createElement('option')        
 
@@ -23,7 +40,7 @@ function adicionar(){
 
         //Números adicionados no ultimo elemento do vetor
         vetorNum.push(numInt)
-        
+
         //MOSTRANDO DADOS VETOR
         /*res.innerHTML = `Vetor ${vetorNum} <br>`
         res.innerHTML += `Inteiro ${numInt} <br>`
@@ -32,32 +49,39 @@ function adicionar(){
 
         //Apaga o que esta no input num 
         num.value = ''
+       
     }
 }
 
 function finalizar(){
-    //Contando quantos elementos tem dentro do vetor
-    let tamanho = vetorNum.length
-    
-    //Maior Valor
-    let maior = Math.max(...vetorNum)
+    //Se o vetor não contiver números dentro dele e tentar clicar em finalizar
+    if( vetorNum.length == 0){
+        window.alert('Adicione valores antes de Finalizar')
 
-    //Menor Valor
-    let menor = Math.min(...vetorNum)
+    } else {
+        //Contando quantos elementos tem dentro do vetor
+        let tamanho = vetorNum.length
+        
+        //Maior Valor
+        let maior = Math.max(...vetorNum)
 
-    //Somando todos os valores do Vetor
-    let soma = 0
-    for(let pos = 0; pos < vetorNum.length ; pos++){
-        soma += vetorNum[pos]
-    }        
+        //Menor Valor
+        let menor = Math.min(...vetorNum)
 
-    //Média dos números   
-    let media = soma / vetorNum.length
+        //Somando todos os valores do Vetor
+        let soma = 0
+        for(let pos in vetorNum){
+            soma += vetorNum[pos]
+        }        
 
-    res.innerHTML = `Ao Todo temos ${tamanho} números cadastrados. <br>`
-    res.innerHTML += `O Maior Número Informador foi ${maior}. <br>`
-    res.innerHTML += `O Menor Número Informador foi ${menor}. <br>`
-    res.innerHTML += `Somando todos os valores, temos ${soma}.<br>` 
-    res.innerHTML += `A média dos valores digitados é ${media}.`
+        //Média dos números   
+        let media = soma / vetorNum.length
 
+        //Resultados impressos na tela
+        res.innerHTML = `Ao Todo temos ${tamanho} números cadastrados. <br>`
+        res.innerHTML += `O Maior Número Informador foi ${maior}. <br>`
+        res.innerHTML += `O Menor Número Informador foi ${menor}. <br>`
+        res.innerHTML += `Somando todos os valores, temos ${soma}.<br>` 
+        res.innerHTML += `A média dos valores digitados é ${media}.`        
+    }
 }
